ef0ca3a051aa0e99cf89071aaf417c693006353e
yefee8 <yavuzefefindikli8@gmail.com>
2022-12-11 18:48:56 +0300

    seo changes

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,9 @@ export default function Home() {
         <meta property="og:title" content="Yefee's portfolio" key="title" />
         <meta property="og:description" content="Hey, I'm Yefee(Yavuz Efe). A ui developer." key="description" />
         <meta property="og:keywords" content="yefee8, yefee, yavuz efe fındıklı, yefee portfolio, yavuz efe findikli" key="keywords" />
+        <meta property="title" content="Yefee's portfolio" key="title" />
+        <meta property="description" content="Hey, I'm Yefee(Yavuz Efe). A ui developer." key="description" />
+        <meta property="keywords" content="yefee8, yefee, yavuz efe fındıklı, yefee portfolio, yavuz efe findikli" key="keywords" />
       </Head>
 
       <Entrance />
