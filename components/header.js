028a52408bc5e311944106d7afca2b23694adbd7
yefee8 <yavuzefefindikli8@gmail.com>
2022-12-11 15:11:15 +0300

    header fix

--- components/header.js
+++ components/header.js
@@ -1,8 +1,7 @@
 export default function Header() {
     return (
         <header className="absolute top-0 left-0 w-full h-36 sm:h-24 flex items-center flex-col sm:flex-row justify-center sm:justify-between gap-y-4 bg-dark z-50">
-            <h1 className="sm:ml-8 text-xl md:text-2xl text-primary cursor-pointer flex relative flex-col">
-                <span className="absolute w-full bg-primary rounded-full opacity-75 h-1 translate-y-[1.1rem] md:translate-y-[1.3rem] md:h-2"></span>
+            <h1 className="sm:ml-8 text-xl md:text-2xl text-primary">
                 Yefee
             </h1>
 
