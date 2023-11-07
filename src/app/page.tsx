d65e481a4d19864ea5ab7e29b8f82e4d6fffd90e
yefee8 <yavuzefefindikli8@gmail.com>
2023-11-07 22:24:32 +0300

    fix: build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ export default function Home() {
   return (
     <main className="flex justify-center gap-4 flex-col items-center w-full h-full absolute left-0 top-0">
       <h1 className="text-3xl text-primary font-bold flex justify-start relative">
-        Hey, I'm Yavuz!
+        Hey, I&apos;m Yavuz!
         <div className="w-full h-2 changeWidth bg-second-primary -z-10 rounded-full absolute translate-y-[2px] bottom-0"></div>
       </h1>
 
       <div className="text-base text-black font-medium text-center container gap-2 flex flex-col">
-        <p>I'm trying to get a scholarship with this website!</p>
+        <p>I&apos;m trying to get a scholarship with this website!</p>
 
         <p>Aaaand if you have the permission(the key) to get in please click to the link.</p>
 
-        <p>If you don't, I'm sorry mate I got nothing for you...</p>
+        <p>If you don&apos;t, I&apos;m sorry mate I got nothing for you...</p>
       </div>
 
       <Link href="/get-in" className="text-base font-medium relative group text-primary">
